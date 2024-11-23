53d0bcfd338c427264931a45be3822067f0cb243
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2024-11-23 13:56:42 +0300

    created models.

--- recipe_site/recipe_app/models.py
+++ recipe_site/recipe_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+    def full_name(self):
+        return f'{self.name} {self.email}'
+
+    def __str__(self):
+        return f"Author: {self.name} email: {self.email}"
+
+    class Meta:
+        verbose_name = "Автор"
+        verbose_name_plural = "Авторы"
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=30)
+    description = models.TextField()
+    ingredients = models.CharField(max_length=100)
+    steps_cooking = models.TextField()
+    time_cooking = models.TextField()
+    image_dish = models.ImageField(upload_to='recipes/')
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=30)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    recipe = models.ManyToManyField(Recipe)
